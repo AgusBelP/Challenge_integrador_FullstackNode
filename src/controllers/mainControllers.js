14fc715c3617e3c78f70e2f929b0b1d57eab8075
AgusBelP <aguspiuzzi@gmail.com>
2023-07-04 23:32:59 -0300

    pruebo controlador pagina contact

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,3 +1,4 @@
 module.exports={
-    main: (req,res) => res.send("Debería devolver la página de home")
+    main: (req,res) => res.send("Debería devolver la página de home"),
+    contact:(req,res) => res.send("Debería devolver la página de contact")
 }
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mainControllers = require('../controllers/mainControllers')
 
 router.get("/", mainControllers.main)
+router.get("/contact", mainControllers.contact)
 
 module.exports = router;
